c1683c43fd52ee4f9b3a420d9c355b5000b2960f
Noitidart <noitidart@gmail.com>
2019-09-02 09:39:48 -0700

    Import all scripts into bundle index

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+import '../dependencies/bootstrap-4/bootstrap-4.css';
+import '../dependencies/fontawesome.css';
+import '../styles/importer.less';
+
+import './cloud.setup';
+import './components/ajax-button.component';
+import './components/ajax-form.component';
+import './components/js-timestamp.component';
+import './components/modal.component';
+import './utilities/open-stripe-checkout';
+import './pages/498.page';
+import './pages/account/account-overview.page';
+import './pages/account/edit-password.page';
+import './pages/account/edit-profile.page';
+import './pages/contact.page';
+import './pages/dashboard/welcome.page';
+import './pages/entrance/confirmed-email.page';
+import './pages/entrance/forgot-password.page';
+import './pages/entrance/login.page';
+import './pages/entrance/new-password.page';
+import './pages/entrance/signup.page';
+import './pages/faq.page';
+import './pages/homepage.page';
+import './pages/legal/privacy.page';
+import './pages/legal/terms.page';
